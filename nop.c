b8533e854eee4f08e9e7546db4953f8e278a0feb
cosmaskiplagat <kiplagatcosmas@hotmail.com>
2023-12-18 12:03:50 -0800

    0x19. C - Stacks, Queues - LIFO, FIFO

---
+++ nop.c
@@ -0,0 +1,9 @@
+#include "monty.h"
+/**
+  nop
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
---
+++ stack.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+/**
+ stack
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
